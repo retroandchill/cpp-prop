761c4e0827d6dd31188a7f01e872f7fb3eaf08af
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-10-01 18:52:22 -0400

    Updated the macros to wrap around methods in the code

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,19 +3,29 @@
 #include "Property/Property.h"
 
 struct ExampleClass {
-    BASIC_PROPERTY(int, property, public, public);
+    PROPERTY_META(ExampleClass);
+
+    PROPERTY(int, property, public, public);
+    PROPERTY(int, dynamic, public, public);
 };
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    ExampleClass example = { 3 };
+    ExampleClass example;
     std::cout << example.property << std::endl;
     example.property = 10;
     std::cout << example.property << std::endl;
     example.property += 2;
     example.property++;
     std::cout << example.property << std::endl;
+
+    std::cout << example.dynamic << std::endl;
+    example.dynamic = 10;
+    std::cout << example.dynamic << std::endl;
+    example.dynamic += 2;
+    example.dynamic++;
+    std::cout << example.dynamic << std::endl;
 
     return 0;
 }
--- include/Property/Property.h
+++ include/Property/Property.h
@@ -5,36 +5,53 @@
 #include <cstddef>
 
 
-#define PROPERTY_META(T) using Self = T;
+#define PROPERTY_META(T) using Self = T
 
-#define PUBLIC_GET()
+#define DEFAULT_GET(T, name) \
+    constexpr T& _get_##name() { \
+        return name.value; \
+    }
 
-#define BASIC_PROPERTY_PRIVATE(T, name, getter_access, const_getter_access, setter_access) \
-    public: \
-    class Property_##name { \
-    public: \
-        template<typename... A> \
-            requires std::constructible_from<T, A...> \
-        constexpr Property_##name(A &&... args) : value(std::forward<A>(args)...) {} \
-        getter_access: \
-        constexpr operator T &() { \
-            return value; \
-        } \
-        const_getter_access: \
-        constexpr operator const T &() const { \
-            return value;\
-        } \
-        setter_access: \
-        template<typename A> \
-            requires std::assignable_from<T, A> \
-        constexpr Property_##name &operator=(A &&arg) { \
-            value = std::forward<A>(arg); \
-            return *this; \
-        } \
-    private: \
-        T value; \
-    }; \
-    Property_##name name
+#define DEFAULT_SET(T, name) \
+    template <typename A> \
+        requires std::assignable_from<T&, A> \
+    constexpr void _set_##name(A&& val) { \
+        name.value = std::forward<A>(val); \
+    }
 
-#define BASIC_PROPERTY(T, name, getter_access, setter_access) BASIC_PROPERTY_PRIVATE(T, name, getter_access, getter_access, setter_access)
+#define PROPERTY_PRIVATE(T, name, getter_access, const_getter_access, getter, setter_access, setter) \
+        public: \
+        class Property_##name { \
+        private: \
+            constexpr Self& getEnclosing() const { \
+                return *reinterpret_cast<Self *>((char *) this - offsetof(Self, ##name)); \
+            } \
+            public: \
+            template <typename... A> \
+                requires std::constructible_from<T, A...> \
+            constexpr Property_##name(A&&... args) : value(std::forward<A>(args)...) {} \
+            getter_access: \
+            constexpr operator T&() { \
+                return getEnclosing()._get_##name(); \
+            } \
+            const_getter_access: \
+            constexpr operator const T&() const { \
+                return getEnclosing()._get_##name(); \
+            } \
+            public: \
+            template <typename A> \
+                requires std::assignable_from<T&, A> \
+            constexpr Property_##name& operator=(A&& val) { \
+                getEnclosing()._set_##name(std::forward<A>(val)); \
+                return *this; \
+            } \
+            private: \
+            friend Self; \
+            int value; \
+        }; \
+        Property_##name name; \
+        getter(T, name) \
+        setter(T, name)
 
+#define PROPERTY(T, name, getter_access, setter_access) \
+    PROPERTY_PRIVATE(T, name, getter_access, getter_access, DEFAULT_GET, setter_access, DEFAULT_SET)
